4c833f1eb934468fa78bd7c9c8d35177c06b95ad
Neil Cecchini <stranger.neil@gmail.com>
2017-06-24 15:50:36 +0200

    Add basic structure
    Initial commit

---
+++ package.lua
@@ -0,0 +1,16 @@
+  return {
+    name = "Emilia",
+    version = "0.0.1",
+    description = "A Discord bot with fish's syntax",
+    tags = { "lua", "lit", "luvit", "discord", "bot" },
+    license = "Copyright",
+    author = { name = "Nodyn", email = "stranger.neil@gmail.com" },
+    homepage = "https://github.com/WhatNodyn/Emilia",
+    dependencies = {
+      "SinisterRectus/discordia"
+    },
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
